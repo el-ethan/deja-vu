dcd59950dc8af5570a3ed442018d19684b85f198
el-ethan <elethan@gmail.com>
2019-01-06 14:06:57 -0600

    Update some stuff

--- database.js
+++ database.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 function connect() {
     mongoose.connect(config.connectionString, {useNewUrlParser: true});
-    var db = mongoose.connection;
+    const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
-        // we're connected!
+        //
     });
     return db;
 }
